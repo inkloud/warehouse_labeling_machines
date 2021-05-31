9878806f9c5354330d293535d99a443d5e57db58
Luca Bacchi <bacchilu@gmail.com>
2021-05-31 09:16:36 +0200

    Printing of a list of ZPL

--- warehouse_labeling_machines/app/label_writer_450/zpl.py
+++ warehouse_labeling_machines/app/label_writer_450/zpl.py
@@ -1,4 +1,4 @@
-from flask import request, make_response
+from flask import request, make_response, jsonify
 
 import libs
 from . import label_writer_450
@@ -12,3 +12,11 @@ def printZPL():
     response = make_response(rawZPL, 200)
     response.mimetype = "text/plain"
     return response
+
+
+@label_writer_450.route("/printZPLList", methods=["POST", "OPTIONS"])
+@libs.cors.crossdomain(origin="*")
+def printZPLList():
+    zplList = request.get_json()
+
+    return jsonify(zplList)
