c5e8935bab2806e35a8ebe3af3f5f11568bdbd84
Luca Bacchi <bacchilu@gmail.com>
2021-10-21 13:46:45 +0200

    Bug fix

--- warehouse_labeling_machines/app/label_writer_450/views.py
+++ warehouse_labeling_machines/app/label_writer_450/views.py
@@ -11,71 +11,78 @@ from PyPDF2 import PdfFileMerger
 from subprocess import run
 
 from . import label_writer_450
-import  time
+import time
+
 
 def createFile(pdfs, filename):
     merger = PdfFileMerger()
     for pdf in pdfs:
         merger.append(pdf)
-    
+
     merger.write(filename)
     merger.close()
 
 
-def printFile(filename, printer_name=""):
-    print('---- START PRINTING ----')
-    return run([".\PDFtoPrinter.exe", filename, printer_name])
-    
-@label_writer_450.route('/', methods=['GET', 'OPTIONS'])
-@libs.cors.crossdomain(origin='*')
+def printFile(filename, printer_name=None):
+    print("---- START PRINTING ----")
+    args = [".\PDFtoPrinter.exe", filename]
+    if printer_name is not None:
+        args.append(printer_name)
+    return run(args)
+
+
+@label_writer_450.route("/", methods=["GET", "OPTIONS"])
+@libs.cors.crossdomain(origin="*")
 def index():
     return "warehouse labeling pinter service active (with printer name)!!!"
 
-@label_writer_450.route('/printPDF', methods=['POST', 'PUT', 'OPTIONS'])
-@libs.cors.crossdomain(origin='*')
+
+@label_writer_450.route("/printPDF", methods=["POST", "PUT", "OPTIONS"])
+@libs.cors.crossdomain(origin="*")
 def printPDF():
 
-    ## save file 
-    print('inizia procedura di stampa')
+    ## save file
+    print("inizia procedura di stampa")
     data = flask.request.get_json()
-    printer = data.get("printer", "")
-    labels = data['label']
-    orderId = data['orderId']
+    printer = data.get("printer", None)
+    labels = data["label"]
+    orderId = data["orderId"]
     startingBox = None
-    if('startingBox' in data):
-        startingBox = data['startingBox']
+    if "startingBox" in data:
+        startingBox = data["startingBox"]
     list_filename = []
-    
+
     ### save files
-    for l in range(0,len(labels)):
-        print('Salva etichetta')
-        b64 = labels[l]['stream']
+    for l in range(0, len(labels)):
+        print("Salva etichetta")
+        b64 = labels[l]["stream"]
         binary = b64decode(b64, validate=True)
         boxToPrint = l
-        if(startingBox is not None):
+        if startingBox is not None:
             boxToPrint = boxToPrint + startingBox
         filename = "{}parcel{}.pdf".format(orderId, boxToPrint)
         list_filename.append(filename)
-        f = open(filename, 'wb')
+        f = open(filename, "wb")
         f.write(binary)
         f.close()
-    
+
     ### merge pdf
 
     completefile = "{}.pdf".format(orderId)
     createFile(list_filename, completefile)
     res = printFile(completefile, printer)
     return "ok"
-    
-@label_writer_450.route('/reprintPDF', methods=['POST', 'PUT', 'OPTIONS'])
-@libs.cors.crossdomain(origin='*')
+
+
+@label_writer_450.route("/reprintPDF", methods=["POST", "PUT", "OPTIONS"])
+@libs.cors.crossdomain(origin="*")
 def reprintPDF():
     data = flask.request.get_json()
-    printer = data.get("printer", "")
-    orderId = data['orderId']
+    printer = data.get("printer", None)
+    orderId = data["orderId"]
 
     rootdir = "."
-    regex = re.compile('{}parcel\d.pdf'.format(orderId))
+    regex = re.compile("{}parcel\d.pdf".format(orderId))
     toPrint = []
     for root, dirs, files in os.walk(rootdir):
         for file in files:
@@ -83,11 +90,10 @@ def reprintPDF():
                 toPrint.append(file)
                 print(file)
 
-    if(len(toPrint) == 0):
+    if len(toPrint) == 0:
         return "No label found", 404
     else:
         completefile = "{}.pdf".format(orderId)
         createFile(toPrint, completefile)
         printFile(completefile, printer)
         return "ok", 200
-
