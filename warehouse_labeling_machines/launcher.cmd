f5b7f4c3ea501807827f74bb66ebc328b0a4e56c
sdg97 <simone.delgatto@life365.eu>
2022-03-01 14:45:12 +0100

    we found a solution for deployment

--- warehouse_labeling_machines/launcher.cmd
+++ warehouse_labeling_machines/launcher.cmd
@@ -1,2 +1,3 @@
 PowerShell -Command "Set-ExecutionPolicy Unrestricted" >> "%TEMP%\StartupLog.txt" 2>&1
+Powershell -Command "Unblock-File .\launch_server.ps1"
 PowerShell  -windowstyle hidden ".\launch_server.ps1" 
